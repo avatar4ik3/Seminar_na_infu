dbf846fe164eb879627d11514d2a4b95e440ae51
Денис Трусенко <crossfirelet@gmail.com>
2019-02-24 14:29:31 +0600

    Класс можно понять

--- Semi/matriza.h
+++ Semi/matriza.h
@@ -3,15 +3,16 @@
 
 class matr {
     private:
-        unsigned m,n;
+        unsigned row,col;
         unsigned **body;
         unsigned err;
+		int swap(unsigned index);
     public:
         matr();
         matr(unsigned, unsigned); //создание
         matr(const matr& sr);     //копирование
         ~matr();                  //уничтожение
-
+// то что писали андрей и максим
         void chel_matr(unsigned i, unsigned j, unsigned elem);    //ввод элемента на определённую позицию
         void back_matr(unsigned i, unsigned j, unsigned &elem);   //вывод изменения на определённую позицую
 
@@ -19,6 +20,8 @@ class matr {
         void print_strok(unsigned &l);
 
         void bad_matr(unsigned err);    // фатальное завершение
+//то что писали денис и никита
+		int shake_sort();
 };
 
 #endif // MATRIZA_H
